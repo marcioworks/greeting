61e945e6f705ea16d1a9bf194c59fc598b894474
marcio silva <marcioadsworks@gmail.com>
2023-02-23 19:33:36 -0300

    start project

---
+++ src/main/java/br/com/marcio/grettingservice/GrettingServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcio.grettingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrettingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrettingServiceApplication.class, args);
+	}
+
+}
